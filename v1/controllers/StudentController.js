eb193c40c25f1e16a563a68018d271ce2a6be6e5
Muhammad Abdul Aziz <zizcode.exporadev@gmail.com>
2018-06-25 09:41:25 +0700

    Modified student api get data

--- v1/controllers/StudentController.js
+++ v1/controllers/StudentController.js
@@ -4,11 +4,38 @@ let StudentController = function(app) {
 	let models = require('../models');
 	let CreateRequest = require('../request/student/CreateRequest');
 
+	let age = function(birth) {
+		let today = new Date();
+		let birthDate = new Date(birth);
+		var age = today.getFullYear() - birthDate.getFullYear();
+		var m = today.getMonth() - birthDate.getMonth();
+		if (m > 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+			age--;
+		}
+		return age;
+	}
+
 	let getAll = function(req, res) {
 		models.Student.findAll()
 		.then(function(result) {
+			let data = [];
+			for(let i = 0; i < result.length; i++){
+				let obj = {
+					id: result[i].id,
+					firstname: result[i].firstname,
+					lastname: result[i].lastname,
+					age: age(result[i].date_of_birth),
+					place_of_birth: result[i].place_of_birth,
+					date_of_birth: result[i].date_of_birth,
+					school: result[i].school,
+					degree: result[i].degree,
+					field_of_study: result[i].field_of_study,
+					bio: result[i].bio
+				}
+				data.push(obj);
+			}
 			res.json({
-				students: result
+				students: data
 			});
 		})
 	}
@@ -21,8 +48,20 @@ let StudentController = function(app) {
 		})
 		.then(function(result) {
 			if (result) {
+				let data = {
+					id: result.id,
+					firstname: result.firstname,
+					lastname: result.lastname,
+					age: age(result.date_of_birth),
+					place_of_birth: result.place_of_birth,
+					date_of_birth: result.date_of_birth,
+					school: result.school,
+					degree: result.degree,
+					field_of_study: result.field_of_study,
+					bio: result.bio
+				}
 				res.json({
-					student: result
+					student: data
 				});
 			}else{
 				res.status(204).json();
